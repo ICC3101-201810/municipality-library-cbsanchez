d317bc63df6c24368f68b87e099cc8ae0d8cef48
Joaquin Alonso <jalonso1@miuandes.cl>
2018-05-22 18:35:24 -0400

    Laboratorio 10

--- Laboratorio10/Laboratorio10/Program.cs
+++ Laboratorio10/Laboratorio10/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using ClassLibrary1;
 
 using System.Runtime.InteropServices;
 
@@ -16,10 +17,13 @@ namespace Laboratorio10
         //private static extern int ClassName(int b);
         static void Main(string[] args)
         {
-            Assembly myassembly;
-            myassembly = Assembly.LoadFile("ClassLibrary1.dll");
 
-            Type[] types = myassembly.GetTypes();
+            //Pregunta 1
+
+            Person persona = new Person();
+            Car auto = new Car();
+            Address clave = new Address();
+            List<Type> types = new List<Type>(persona.GetType(), auto.GetType(), clave.GetType());
 
             foreach (Type t in types)
             {
@@ -35,12 +39,99 @@ namespace Laboratorio10
                     {
                         Console.WriteLine("method info: {0}\n" + t.GetMethod(m.Name));
                     }
+                    PropertyInfo[] properties = t.GetProperties();
+                    foreach (PropertyInfo property in properties)
+                    {
+                        Console.WriteLine("Property Name: {0}\n", property.Name);
+                        Console.WriteLine("Property Type Name: {0}\n", property.PropertyType.Name);
+                        Console.WriteLine("Property Type FullName: {0}\n", property.PropertyType.FullName);
+                        Console.WriteLine("Can we read the property?: {0}\n", property.CanRead.ToString());
+                        Console.WriteLine("Can we write the property?: {0}\n", property.CanWrite.ToString());
+                    }
                 }
                 catch (System.NullReferenceException)
                 {
                     Console.WriteLine("Archivo vacio");
                 }
             }
+
+
+
+            // Pregunta 2
+            List<Person> personas = new List<Person>();
+            List<Car> autos = new List<Car>();
+            List<Address> Propiedades = new List<Address>();
+            while (true)
+            {
+                Console.WriteLine("menu:");
+                Console.WriteLine("1.Inscribir persona");
+                Console.WriteLine("2.Inscribir Propiedad");
+                Console.WriteLine("3.Inscribir automovil");
+                Console.WriteLine("4.Salir");
+                string eleccion = Console.ReadLine();
+                if (eleccion == "1")
+                {
+                    Console.WriteLine("Ingrese el Primer nombre de la persona:");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Ingrese el Apellido de la persona:");
+                    string LastNAme = Console.ReadLine();
+                    Console.WriteLine("Ingrese el rut de la persona:");
+                    string rut = Console.ReadLine();
+                    Console.WriteLine("Ingrese el cumpleaños de la persona:");
+                    DateTime datebirth = Convert.ToDateTime(Console.ReadLine());
+                    Console.WriteLine("Ingrese la calle de la direccion:");
+                    string Street = Console.ReadLine();
+                    Console.WriteLine("Ingrese el numero de la calle de la direccion:");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    Address propiedad;
+                    foreach (Address a in Propiedades)
+                    {
+                        if ((a.Street == Street) & (a.Number == number))
+                        {
+                            propiedad = a;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Ingrese el rut del primer pariente:");
+                    string rut1 = Console.ReadLine();
+                    Console.WriteLine("Ingrese el rut del segundo pariente");
+                    string rut2 = Console.ReadLine();
+                    Person person1;
+                    Person person2;
+                    foreach (Person p in personas)
+                    {
+                        if (rut1 == p.Rut)
+                        {
+                            person1 = p;
+                            break;
+                        }
+                    }
+                    foreach (Person p in personas)
+                    {
+                        if (rut2 == p.Rut)
+                        {
+                            person2 = p;
+                            break;
+                        }
+                    }
+                    Person personanueva = new Person(firstName, LastNAme, datebirth, propiedad, rut, person1, person2);
+                    personas.Add(personanueva);
+
+                }
+                if (eleccion == "2")
+                {
+                    Console.WriteLine("");
+                }
+                if (eleccion == "3")
+                {
+
+                }
+                if (eleccion == "4")
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
